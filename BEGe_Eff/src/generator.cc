85d50c313119833c34a8a5cd1379def2eb298160
RiPires-nucria <fc49807@alunos.fc.ul.pt>
2024-06-05 11:21:17 +0100

    RNG investigating seed

--- BEGe_Eff/src/generator.cc
+++ BEGe_Eff/src/generator.cc
@@ -1,4 +1,5 @@
 #include "generator.hh"
+#include "Randomize.hh"
 
 
 //   Primary particle generator constuctor and destructor   //
@@ -9,34 +10,11 @@ MyPrimaryGenerator::MyPrimaryGenerator()
     G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
     G4String particleName="geantino"; // Sets prim .particle as GEANTINO
     G4ParticleDefinition *particle = particleTable->FindParticle("geantino"); 
-    
-<<<<<<< HEAD
-<<<<<<< HEAD
-    G4ThreeVector pos(0.,0.,-19.5*mm+100*nm); //  Position for particle gun
-    G4ThreeVector mom(0.,0.,1.);     //  Momentum direction vector for prim. particle
-    
-    fParticleGun->SetParticlePosition(pos);          // Particle Position
-    fParticleGun->SetParticleMomentumDirection(mom); // Particle Momentum Direction
-    fParticleGun->SetParticleMomentum(0.*keV);       // Particle Momentum magnitude
-    fParticleGun->SetParticleDefinition(particle);   // Sets particle as GEANTINO prev. deffined
-=======
-    // Set isotropoic distribution //
-    G4double twopi = 6.28318530718;
-    G4double cosTheta = -1.0 + 2.0*G4UniformRand();
-    G4double phi = twopi * G4UniformRand();
-    G4double sinTheta = sqrt(1 - cosTheta*cosTheta);
 
-  //  Momentum direction vector for prim. particle
-    fParticleGun->SetParticleMomentumDirection(G4ThreeVector(sinTheta*cos(phi), sinTheta*sin(phi), cosTheta)); // Particle Momentum Direction
-
-    G4ThreeVector pos(0.,0.,-20.);                    //  Position for particle gun
-=======
-    G4ThreeVector pos(0.,0.,-20.);                   //  Position for particle gun
->>>>>>> aec6492... RNG Investigating the seed
+    G4ThreeVector pos(0.,0.,-20.);                  //  Position for particle gun
     fParticleGun->SetParticlePosition(pos);         // Particle Position
     fParticleGun->SetParticleMomentum(0.*MeV);      // Particle Momentum magnitude
     fParticleGun->SetParticleDefinition(particle);  // Sets particle as GEANTINO prev. deffined
->>>>>>> d21084e... BEGe_Eff update:
 }
 
 MyPrimaryGenerator::~MyPrimaryGenerator()
@@ -50,20 +28,6 @@ void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
     G4ParticleDefinition *particle = fParticleGun->GetParticleDefinition();
     
     if(particle == G4Geantino::Geantino())
-<<<<<<< HEAD
-    {
-        // 117Sb source //
-        G4int Z = 51;
-        G4int A = 117;
-        
-        G4double charge = 0.;
-        G4double energy = 0.;
-        
-        G4ParticleDefinition *ion = G4IonTable::GetIonTable()->GetIon(Z, A, energy);
-        
-        fParticleGun->SetParticleDefinition(ion);
-        fParticleGun->SetParticleCharge(charge);
-=======
     {   
         G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
         G4ParticleDefinition *gamma = particleTable->FindParticle("gamma");
@@ -77,9 +41,7 @@ void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
 
         //  Momentum direction vector for prim. particle
         fParticleGun->SetParticleMomentumDirection(G4ThreeVector(sinTheta*cos(phi), sinTheta*sin(phi), cosTheta)); // Particle Momentum Direction
-
         fParticleGun->SetParticleDefinition(gamma);
->>>>>>> d21084e... BEGe_Eff update:
     }
 
     fParticleGun->GeneratePrimaryVertex(anEvent);
