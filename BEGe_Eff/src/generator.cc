d9ac0c31325154d3f6293123d71e531e34cf5bea
RiPires-nucria <fc49807@alunos.fc.ul.pt>
2024-07-15 14:57:54 +0100

    Isotropic distribution achieved:
    	* using G4GeneralParticleSource() instead of G4ParticleGun();
    	* the definition of source properties is made using a macro;

--- BEGe_Eff/src/generator.cc
+++ BEGe_Eff/src/generator.cc
@@ -1,19 +1,20 @@
 #include "generator.hh"
 #include "Randomize.hh"
+#include "G4RandomDirection.hh"
 
 
 //   Primary particle generator constuctor and destructor   //
 MyPrimaryGenerator::MyPrimaryGenerator()
 {
-    fParticleGun = new G4ParticleGun(1);
+    fParticleGun = new G4GeneralParticleSource();
     
     G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
     G4String particleName="geantino"; // Sets prim .particle as GEANTINO
     G4ParticleDefinition *particle = particleTable->FindParticle("geantino"); 
 
-    G4ThreeVector pos(0.,0.,-20.);                  //  Position for particle gun
-    fParticleGun->SetParticlePosition(pos);         // Particle Position
-    fParticleGun->SetParticleMomentum(0.*MeV);      // Particle Momentum magnitude
+    //G4ThreeVector pos(0.,0.,-20.);                  //  Position for particle gun
+    //fParticleGun->SetParticlePosition(pos);         // Particle Position
+    //fParticleGun->SetParticleMomentum(0.*MeV);      // Particle Momentum magnitude
     fParticleGun->SetParticleDefinition(particle);  // Sets particle as GEANTINO prev. deffined
 }
 
@@ -31,17 +32,11 @@ void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
     {   
         G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
         G4ParticleDefinition *gamma = particleTable->FindParticle("gamma");
-        fParticleGun->SetParticleMomentum(25.*keV);
-        
-        // Set isotropoic distribution //
-        G4double twopi = 6.28318530718;
-        G4double cosTheta = -1.0 + 2.0*G4UniformRand();
-        G4double phi = twopi * G4UniformRand();
-        G4double sinTheta = sqrt(1 - cosTheta*cosTheta);
-
-        //  Momentum direction vector for prim. particle
-        fParticleGun->SetParticleMomentumDirection(G4ThreeVector(sinTheta*cos(phi), sinTheta*sin(phi), cosTheta)); // Particle Momentum Direction
         fParticleGun->SetParticleDefinition(gamma);
+        //fParticleGun->SetParticleMomentum(158.*keV);
+
+        G4ThreeVector direction = G4RandomDirection();
+        //fParticleGun->SetParticleMomentumDirection(direction); 
     }
 
     fParticleGun->GeneratePrimaryVertex(anEvent);
