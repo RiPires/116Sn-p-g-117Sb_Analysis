922989593075f45d8254c87894cf6ed4aefc86a1
RiPires-nucria <fc49807@alunos.fc.ul.pt>
2024-11-21 14:18:13 +0000

    DetSim root-scripts update: 	* new PlotMultiple.C - for ploting arbitrary multiple sim files and compare with exp data; 	* new FitPeak.C and PlotBothAndFit.C - perform gaussian fit to a specified Region of Interest, returns peak integral, and then prints sim to exp peak ratio for each calibration run;

--- DetSim/SDD_Eff/root-scripts/FitPeak.C
+++ DetSim/SDD_Eff/root-scripts/FitPeak.C
@@ -1,5 +1,47 @@
 #include <iostream>
+#include "TF1.h"
 
-void FitPeak(double fitMin, double fitMax) {
-    TF1 *gausFit = new TF1('gausFit', 'gaus', fitMin, fitMax);
+using namespace std;
+
+// Funtion to perform gaussian fit and extract fit parameters
+vector<double> FitPeak(TH1D *hist, const vector<double> &fitMin, const vector<double> &fitMax) {
+
+    //cout << "Fitting peaks for histogram: " << hist->GetName() << endl;
+
+    // Vectors to store fit parameters
+    vector<double> cents;
+    vector<double> sigmas;
+    vector<double> integrals; 
+    // Loop over each ROI
+    for (size_t i = 0; i < fitMin.size(); ++i){
+        
+        double ROId = fitMin[i];
+        double ROIu = fitMax[i];
+
+        // Define Gaussian fit function
+        string fitName = "gausFit" + to_string(i);
+        TF1 *gaussFit = new TF1(fitName.c_str(), "gaus", ROId, ROIu);
+
+        // Perform the fit
+        hist->Fit(gaussFit, "RQ"); // R = Range, Q = Quiet mode
+
+        // Extract fit parameters
+        double centroid = gaussFit->GetParameter(1);        // Mean (centroid)
+        double sigma = gaussFit->GetParameter(2);           // Standard deviation
+        double amplitude = gaussFit->GetParameter(0);       // Amplitude
+        double integral = amplitude * sigma * sqrt(2*M_PI); // Integral
+
+        // Push back to output vectors
+        cents.push_back(centroid);
+        sigmas.push_back(sigma);
+        integrals.push_back(integral);
+
+        // Print results
+        //cout << "Peak " << i+1 << " results:" << endl;
+        //cout << "Centroid: " << centroid << " MeV" << endl;
+        //cout << "Sigma: " << sigma << " MeV" << endl;
+        //cout << "Integral: " << integral << " counts" << endl;
+        //cout << "-----------------\n" << endl;
+    }
+    return integrals;
 }
