1d6bbac654ccd70a2c94de6c2891f54920644185
RiPires <fc49807@alunos.fc.ul.pt>
2026-07-21 13:45:56 +0100

    PlotCalib using background subtracted

--- CNA/Scripts-CNA/PlotCalib.py
+++ CNA/Scripts-CNA/PlotCalib.py
@@ -15,15 +15,16 @@ y, ch = Ge2Lists(calibHPGeFile)
 PlotLogy(ch, y, calibHPGeFile.replace('../Calibrations/HPGe/CalibrationRuns_PosExp/','')) """
 
 ## HPGe detector path
-calibPathHPGe = '../Calibrations/HPGe/CalibrationRuns_PosExp/'
+calibPathHPGe = '../Calibrations/HPGe/CalibrationRuns_PosExp/BgRemoved/'
 ## Loop over HPGe calibration runs
 for file in os.listdir(calibPathHPGe):
 
     if (str(file) == "BgRemoved"):
         pass
     else:            
-        y, ch = Ge2Lists(str(calibPathHPGe+file))
-        PlotLogy(ch, y, str(file).replace('.mca', ''))
+        y, ch = Ge2ListsBgRm(str(calibPathHPGe+file))
+        energies = [(ch[i]*0.322526-0.45563) for i in range(len(ch))]
+        PlotLogy(energies, y, str(file).replace('.mca', ''))
 
 ## SDD detector path
 calibPathSDD = '../Calibrations/SDD/CalibrationRuns_PosExp/'
